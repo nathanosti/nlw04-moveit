a0d33b7dce5b9ed940c808b038a6b2e930ecceac
Nathan Osti <nathanosti.job@gmail.com>
2021-03-05 17:09:25 -0300

    Create countdown part01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from '../components/Profile';
 import CompletedChallenges from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
 
 import styles from '../styles/pages/Home.module.css';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home | MoveIt</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div className={styles.leftContainer}>
           <Profile />
           <CompletedChallenges />
-
+          <Countdown />
         </div>
 
         <div>
