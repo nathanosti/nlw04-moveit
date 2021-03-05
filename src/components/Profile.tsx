ff1e9b8bc77c3c7d07ae7a45a6b84e19583aa278
Nathan Osti <nathanosti.job@gmail.com>
2021-03-05 11:46:25 -0300

    Create Profile Component and style to Home Page

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return(
+    <div>
+      <img src="https://github.com/nathanosti.png" />
+      <div>
+        <strong>Nathan Osti</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
