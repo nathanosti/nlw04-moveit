cdbdf8f2f4425a2a00ee676d39ab31d322faa4a2
Nathan Osti <nathanosti.job@gmail.com>
2021-03-09 18:07:15 -0300

    Add audio notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -48,6 +48,8 @@ export function ChallengesProvider({ children }: ChallengerProviderProps) {
 
     setActiveChallenge(challenge);
 
+    new Audio('/notification.mp3').play();
+
     if (Notification.permission === 'granted') {
       new Notification('Novo Desafio', {
         body: `Valendo ${challenge.amount} xp!`
