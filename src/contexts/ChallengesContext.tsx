9657e257c93a0fd9961efe9caa26444ea1a30c09
Nathan Osti <nathanosti.job@gmail.com>
2021-03-09 17:59:57 -0300

    Create notifications to a new challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json'
 
 
@@ -33,16 +33,26 @@ export function ChallengesProvider({ children }: ChallengerProviderProps) {
   const [activeChallenge, setActiveChallenge] = useState(null);
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
-  
+
+  useEffect(() => {
+    Notification.requestPermission();
+  }, [])
+
   function levelUp() {
     setLevel(level + 1);
   }
   
   function startNewChallenge() {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
-    const challenge = challenges[randomChallengeIndex];
+    const challenge  = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo Desafio', {
+        body: `Valendo ${challenge.amount} xp!`
+      })
+    }
   }
 
   function completeChallenge() {
